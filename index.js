b7a2e8637cc835edf665a8378c844d4938e3dd2d
mukitcoder <mukitbd71@gmail.com>
2021-11-11 22:52:06 +0600

    add users

--- index.js
+++ index.js
@@ -18,14 +18,46 @@ async function run() {
       await client.connect();
       const database = client.db('sunglass_pro');
       const purchaseCollection = database.collection('purchase');
-// post purchase info to DB
+      const usersCollection = database.collection('users')
+
+        // post purchase info to DB
         app.post('/purchase', async(req, res)=>{
             const purchases = req.body
             const result = await purchaseCollection.insertOne(purchases)
-            console.log(result);
+            res.json(result)
+        })
+        // users info
+        app.post('/users', async(req, res)=>{
+            const user = req.body
+            const result = await usersCollection.insertOne(user)
             res.json(result)
         })
 
+        app.put('/users', async(req, res)=>{
+            const user = req.body
+            const filter = {email:user.email}
+            const options = {upsert:true}
+            const updateDoc = {$set:user}
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            res.json(result)
+        })
+
+        app.put('/users/admin', async(req, res)=>{
+            const user = req.body
+            const filter = {email:user.email}
+            const updateDoc = {$set:{role:'admin'}}
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.json(result)
+        })
+
+        // get purchase info from DB
+        app.get('/purchase', async (req, res)=>{
+            const email = req.query.email
+            const query = {email:email}
+            const cursor = purchaseCollection.find(query)
+            const orders = await cursor.toArray()
+            res.json(orders)
+        })
 
     } finally {
     //   await client.close();
