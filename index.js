d6b5876397a82b060efc6a8752c9ac55e37634ae
mukitcoder <mukitbd71@gmail.com>
2021-11-12 14:29:47 +0600

    get allProducts

--- index.js
+++ index.js
@@ -18,8 +18,16 @@ async function run() {
       await client.connect();
       const database = client.db('sunglass_pro');
       const purchaseCollection = database.collection('purchase');
+      const productsCollection = database.collection('products')
       const usersCollection = database.collection('users')
 
+
+
+      // add products to DB
+      app.post('/addProducts', async(req, res)=>{
+        const result = await productsCollection.insertOne(req.body)
+        res.send(result)
+      })
         // post purchase info to DB
         app.post('/purchase', async(req, res)=>{
             const purchases = req.body
@@ -42,12 +50,19 @@ async function run() {
             res.json(result)
         })
 
+        // for make admin
         app.put('/users/admin', async(req, res)=>{
             const user = req.body
             const filter = {email:user.email}
             const updateDoc = {$set:{role:'admin'}}
             const result = await usersCollection.updateOne(filter, updateDoc)
             res.json(result)
+        })
+
+        //get all products from db
+        app.get('/allProducts', async(req, res)=>{
+          const result = await productsCollection.find({}).toArray()
+          res.send(result)
         })
 
         // get purchase info from DB
@@ -57,6 +72,18 @@ async function run() {
             const cursor = purchaseCollection.find(query)
             const orders = await cursor.toArray()
             res.json(orders)
+        })
+
+        // for check admin to get access in UI
+        app.get('/users/:email', async(req, res)=>{
+          const email = req.params.email
+          const query = {email:email}
+          const user = await usersCollection.findOne(query)
+          let isAdmin = false
+          if(user?.role === 'admin'){
+              isAdmin = true
+          }
+          res.json({admin:isAdmin})
         })
 
     } finally {
